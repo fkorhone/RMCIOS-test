9f3407d6e1f94da5b4e27c40a7acd629dadf7548
Frans Korhonen <frans.korhonen@rmcios.fi>
2021-01-24 07:27:43 +0200

    Added macro for string asserts.
    Improved regular type error printout to be summetrical:
    X = value1 != Y = value2

--- test.h
+++ test.h
@@ -23,13 +23,20 @@ int test_stage=0;
                                                printf( "PASSED: %s:%d %s\n",__FILE__ , __LINE__, test_label); } 
 
 #define TEST_ASSERT_EQUAL_EX(EXPECTED, GOT, CAST_TO, PRINT_TYPE) if ((CAST_TO)EXPECTED != (CAST_TO)GOT){ test_status[test_index] = FAIL; \
-                                               printf( "FAILED: %s:%d %s "  #EXPECTED " = " PRINT_TYPE " != " #GOT " %s\n",__FILE__ , __LINE__, test_label, (CAST_TO)EXPECTED, test_description); } \
+                                               printf( "FAILED: %s:%d %s "  #EXPECTED " = " PRINT_TYPE " != " #GOT " = " PRINT_TYPE " %s\n",\
+                                               __FILE__ , __LINE__, test_label, (CAST_TO)EXPECTED, (CAST_TO)GOT, test_description); } \
                                          else { if(test_status[test_index] != FAIL) test_status[test_index] = PASS; \
                                                printf( "PASSED: %s:%d %s\n",__FILE__ , __LINE__, test_label); } 
 
 #define TEST_ASSERT_EQUAL_INT(EXPECTED, GOT)  TEST_ASSERT_EQUAL_EX(EXPECTED, GOT, int, "%d")
 #define TEST_ASSERT_EQUAL_CHAR(EXPECTED, GOT)  TEST_ASSERT_EQUAL_EX(EXPECTED, GOT, char, "'%c'")
 #define TEST_ASSERT_EQUAL_FLOAT(EXPECTED, GOT)  TEST_ASSERT_EQUAL_EX(EXPECTED, GOT, float, "%f")
+
+#define TEST_ASSERT_EQUAL_STR(EXPECTED, GOT)  if (strcmp(EXPECTED, GOT) != 0){ test_status[test_index] = FAIL; \
+                                               printf( "FAILED: %s:%d %s "  #EXPECTED " = \"%s\" != " #GOT " = \"%s\" %s\n",\
+                                               __FILE__ , __LINE__, test_label, EXPECTED, GOT, test_description); } \
+                                         else { if(test_status[test_index] != FAIL) test_status[test_index] = PASS; \
+                                               printf( "PASSED: %s:%d %s\n",__FILE__ , __LINE__, test_label); } 
 
 #define INIT_TESTING(TEST_CASES) total_tests = TEST_CASES; char test_status_data[TEST_CASES] = {0}; test_status = test_status_data;
 
